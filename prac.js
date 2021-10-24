762ec2f4db01a6b63376ee450eb801a1accdae10
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-24 18:34:23 +0530

    bigint and null

--- prac.js
+++ prac.js
@@ -1,25 +1,12 @@
 "use strict"
-// convert number to sting 
-let age=56;
-String(age);
-age=age+'';
-console.log(typeof age)
-
-// convert sting to number 
-var mystr='34';
-console.log(typeof mystr)
-mystr=+'34';
-console.log(typeof mystr)
-var mystr='34';
-mystr=Number(mystr)
-console.log(typeof mystr)
-
-// template string 
-let age1=9;
-let name = 'Ibrahim';
-// normal way:
-let aboutme1='my name is '+name+' and my age is '+age;
-console.log(aboutme1);
-// template string 
-let aboutme = `my name is ${name} and my age is ${age}`
-console.log(aboutme)
+// BigInt and NUll 
+const first='k';
+console.log(first);
+let my=null;
+console.log(typeof my)
+let num=21n;
+console.log(typeof num);
+console.log(Number.MAX_SAFE_INTEGER);
+let n1=7;
+let n2="7"
+console.log(n2!=n1)
