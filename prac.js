7bb00aa933c72d283b7ce7cb708d3567d91abac1
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-24 19:01:24 +0530

    get,set and clone

--- prac.js
+++ prac.js
@@ -1,11 +1,25 @@
 "use strict"
-// ternary operator
-let n1=7;
-let n2="7";
-let k=n1===n2?'hello':'bye';
-console.log(k)
-let i=0;
-while(i<3){
-    console.log(i)
-    i++;
+// =====================================================
+                //   MAP 
+// =====================================================
+const person = new Map([['firstName','Mohammad'],['age',90]]);
+person.set('firstname','ibrahim');
+person.set('ages',20);
+person.set([1,2,3],'onetwothree')
+console.log(person)
+console.log(person.get('age'))
+for(let i of person){
+    console.log(i[0])
 }
+for(let [i,j] of person){
+    console.log(i,j)
+}
+// --------------------------------------
+// clone using object assign
+// m-1
+const obj={'yo':21}
+const obj2={...obj}
+console.log(obj2)
+// m-2
+const onj2=Object.assign({},obj)
+console.log(obj)
