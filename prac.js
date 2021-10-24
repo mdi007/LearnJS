b9be54fd46baaea002b26f4ab30e95a5c707f09d
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-24 18:31:24 +0530

    conversions and template string

--- prac.js
+++ prac.js
@@ -1,17 +1,25 @@
 "use strict"
-var Name='Mohammad Ibrahim'
-console.log(Name.length)
-var Name='     Mohammad Ibrahim          ';
-console.log(Name.length)
-console.log(Name.trim())
-console.log(Name.toUpperCase())
-console.log(Name.toLowerCase())
-console.log(Name.slice(10,20))
-console.log(Name.indexOf('d'));
-console.log(Name.lastIndexOf('i'))
-console.log(Name.includes('k'));
-var Name='Mohammad Ibrahim'
-console.log(Name.split(' '));
-console.log(Name.replace(' ',','));
-console.log(Name.charAt(5));
-console.log(Name.substring(5,11));
+// convert number to sting 
+let age=56;
+String(age);
+age=age+'';
+console.log(typeof age)
+
+// convert sting to number 
+var mystr='34';
+console.log(typeof mystr)
+mystr=+'34';
+console.log(typeof mystr)
+var mystr='34';
+mystr=Number(mystr)
+console.log(typeof mystr)
+
+// template string 
+let age1=9;
+let name = 'Ibrahim';
+// normal way:
+let aboutme1='my name is '+name+' and my age is '+age;
+console.log(aboutme1);
+// template string 
+let aboutme = `my name is ${name} and my age is ${age}`
+console.log(aboutme)
