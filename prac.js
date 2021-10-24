fb6b3b26b705d55559680266cc22c960201e3ecd
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-24 18:37:14 +0530

    ternary operator and while loop

--- prac.js
+++ prac.js
@@ -1,6 +1,11 @@
-// // falsy values
-" "
-null
-undefined
-0
-falese 
+"use strict"
+// ternary operator
+let n1=7;
+let n2="7";
+let k=n1===n2?'hello':'bye';
+console.log(k)
+let i=0;
+while(i<3){
+    console.log(i)
+    i++;
+}
