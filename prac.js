a4d076398abeb787809786b08146b17412f7da08
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-24 18:35:21 +0530

    falsy values

--- prac.js
+++ prac.js
@@ -1,12 +1,6 @@
-"use strict"
-// BigInt and NUll 
-const first='k';
-console.log(first);
-let my=null;
-console.log(typeof my)
-let num=21n;
-console.log(typeof num);
-console.log(Number.MAX_SAFE_INTEGER);
-let n1=7;
-let n2="7"
-console.log(n2!=n1)
+// // falsy values
+" "
+null
+undefined
+0
+falese 
