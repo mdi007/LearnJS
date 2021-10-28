9cb2bdc2984a6fec0848acf8c66984901aef078d
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-28 19:21:03 +0530

    call,apply,bind

--- prac.js
+++ prac.js
@@ -1,12 +1,25 @@
 "use strict"
-// function inside object
-
-function personinfo(){
-    console.log(this.firstName,this.address);
-}
+// call,apply,bind
 const user={
     firstName:'ibrahim',
     address:{home:'30-27'},
-    about:personinfo
+    about:function(hobby){console.log(` my hobby is ${hobby}`)}
 }
-user.about()
+const user2={
+    firstName:'mohammad',
+    age:9
+}
+function about1(hobby){console.log(` my hobby is ${hobby}`)}
+// call-basically we can use same function for differrent users
+user.about('playing')
+user.about.call(user2,'dancing')
+// if about is outside of user then :- about.call(user2)
+// ----------------
+                // apply
+about1.apply(user,['yoy','dance'])
+// in apply we can pass array and it takes number of parameters
+// ----------------
+//                  bind 
+// it returns a functions
+const func=about1.bind(user2,'dance');
+func();
