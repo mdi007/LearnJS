2f9090658e2d73bd09464844a9b197d12cd315b1
Mohammad Ibrahim <mdibrahim26201@gmail.com>
2021-10-28 18:49:36 +0530

    optimal chaining..

--- prac.js
+++ prac.js
@@ -1,11 +1,8 @@
 "use strict"
-// ternary operator
-let n1=7;
-let n2="7";
-let k=n1===n2?'hello':'bye';
-console.log(k)
-let i=0;
-while(i<3){
-    console.log(i)
-    i++;
+// optimal chaining
+const user={
+    firstName:'ibrahim',
+    address:{home:'30-27'}
 }
+console.log(user?.firstName);
+console.log(user?.address?.home);
